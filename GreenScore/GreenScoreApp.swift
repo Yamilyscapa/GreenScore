33fd04d02555476c0c3e5fe3e4c5f70016507391
yam <yamil.yscapa@gmail.com>
2025-04-01 16:48:29 -0600

    Initial Commit

---
+++ GreenScore/GreenScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenScoreApp.swift
+//  GreenScore
+//
+//  Created by Yamil Yscapa on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct GreenScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
