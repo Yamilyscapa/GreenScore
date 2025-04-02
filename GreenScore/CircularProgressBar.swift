403422dbcbb17046dcace828178443a0c7bc251c
yam <yamil.yscapa@gmail.com>
2025-04-02 11:30:09 -0600

    Add CircularProgressBar component and added it to HomeView

---
+++ GreenScore/CircularProgressBar.swift
@@ -0,0 +1,43 @@
+//
+//  CircularProgressBar.swift
+//  GreenScore
+//
+//  Created by Yamil Yscapa on 02/04/25.
+//
+
+import SwiftUI
+
+struct CircularProgressBar: View {
+    var progress: CGFloat
+
+    var body: some View {
+        HStack(spacing: 52) {
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 25)
+                    .opacity(0.3)
+                    .foregroundColor(.gray).opacity(0.50)
+
+                Circle()
+                    .trim(from: 0.0, to: progress)
+                    .stroke(Color("MainColor"), lineWidth: 25)
+                    .rotationEffect(Angle(degrees: -90))
+                    .animation(.linear(duration: 1.0), value: progress)
+            }
+            .frame(width: 140, height: 140)
+
+            VStack(alignment: .leading) {
+                Text("Reduction").font(.system(size: 16, weight: .light))
+                    .foregroundColor(.gray).padding(.bottom, 5)
+
+                Text("25%").font(.system(size: 28, weight: .bold))
+
+                CustomButton(buttonText: "Log", isSmall: true)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
--- GreenScore/HomePage.swift
+++ GreenScore/HomePage.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        VStack {
-            Text("Home").font(.system(size: 20, weight: .bold))
-            CustomButton(buttonText: "Log", isSmall: true)
-            CustomButton(buttonText: "Click me")
 
+    var streak: Int = 10
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            ViewTitle(streak: streak).padding(.top, 40)
+            CircularProgressBar(progress: 0.25).padding(.top, 24)
+                Spacer()
+            }
+    }
+
+    struct ViewTitle: View {
+
+        var streak: Int
+
+        var body: some View {
+                HStack() {
+                    Text("Streak -")
+                        .font(
+                            .system(
+                                size: 24,
+                                weight: .medium)
+                        )
+                    Text("\(streak) days")
+                        .font(
+                            .system(
+                                size: 24,
+                                weight: .medium)
+                        ).foregroundStyle(Color("MainColor"))
+                }
         }
     }
+}
+
+#Preview {
+    HomeView()
 }
